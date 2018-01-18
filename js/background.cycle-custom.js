df3c3e1b46db8e57c265d9b3baa1eb3889346c46
Ayobami <prudentprogrammer@gmail.com>
2018-01-18 17:01:57 +0100

    initial commit

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var fadeShow = $(".background").fadeShow({
+
+	correctRatio: true,
+
+	shuffle: true,
+
+	speed: 3500,
+
+	images: [
+		'images/bg-image.jpg',
+		'images/bg-image-3.jpg',
+		'images/bg-image-4.jpeg'
+
+	]
+
+
+
+});
